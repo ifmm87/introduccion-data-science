d8c11a69fa8a4613b69f6fb38e3ff8ae69c08e14
Ivan Mujica <you@example.com>
2020-09-15 01:46:28 -0400

    first commit first exercise

--- exercises-1109/word-generator.py
+++ exercises-1109/word-generator.py
@@ -1,10 +1,20 @@
-print(f'hello')
-#  from random import choices
+print(f'+++++++++++++++++++GENERATING AWFUL POETRY++++++++++++++++++++++')
+#  import random from random
 import random
-articles = ['the', 'of', 'from', 'by']
-subject = [ 'cat', 'puppy','friend', 'lover', 'woman' ]
-verbs = ['eat', 'love', 'run away', 'show', 'pick']
-adverbs = ['louddly', 'well', 'badly','always', 'quietly']
-for n in range(5) :
-
-    print(n, random.randint(0, 5))
+# declaring arrays
+articles = ['the', 'an', 'a', 'those', 'that', 'these', 'this']
+subjects = [ 'cat', 'puppy','friend', 'lover', 'woman', 'children', 'parents', 'relatives', 'neighibors' ]
+verbs = ['eat', 'loves', 'run away', 'shows', 'picks', 'sit down', 'end up', 'turn out' ]
+adverbs = ['loudly', 'well', 'badly','always', 'quietly', '']
+# Entering required number of awful verses
+inp = input('Enter how many awful poetry verses you need: ')
+# Iterating until inp number of verses
+try:
+    for n in range(int(inp)) :
+        if random.randint(0,1) == 1 :
+            print(f'{n + 1} {random.choice(articles)} {random.choice(subjects)} {random.choice(verbs)} {random.choice(adverbs)}')
+        else :
+            print(f'{n + 1} {random.choice(articles)} {random.choice(subjects)} {random.choice(verbs)}')
+    pass
+except ValueError as  error:
+    print('You have not entered a number!.')
